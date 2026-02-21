63f6a90345cd1d7b8f56d498004a9b191766c5fb
Indika Madhusankha <indikamadushanka91@gmail.com>
2026-02-22 00:59:44 +0530

    login page updated

--- resources/frontend/src/components/signIn/SignInForm.tsx
+++ resources/frontend/src/components/signIn/SignInForm.tsx
@@ -3,75 +3,46 @@ import React from "react";
 const SignInForm: React.FC = () => {
     return (
 
-            <div className="xl:bg-cover bg-no-repeat bg-[url(assets/images/background.jpg)] " >
-            <div className="mx-auto mt-[20vh]">
-                <div className="">
-                    <form className="max-w-sm mx-auto fill-white drop-shadow-xl/50 rounded-xl p-5 bg-white opacity-85">
-                        <p className="my-5 text-center text-2xl font-medium">
-                            LOGIN
-                        </p>
-                        <div className="mb-5">
-                            <label
-                                htmlFor="email"
-                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                            >
-                                Email Address
-                            </label>
-                            <input
-                                type="email"
-                                id="email"
-                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                placeholder="Email Address"
-                                required
-                            />
+
+        <div id="content" className="max-w-screen-lg mx-auto p-2 md:py-3 mt-[20vh]">
+            <div id="login" className="shadow-xl/30 w-[400px] mx-auto p-5 rounded-xl">
+                <form action="" method="POST">
+                    <h2 className="py-4 text-2xl font-semibold text-center">User Login</h2>
+                    <div>
+                        <label className="flex" htmlFor="email">Email Address:</label>
+                        <input className="flex w-full p-2 my-1 rounded-lg shadow-xl ring ring-blue-500/50"
+                               type="text" name="email" id="email" value=""/>
+                        <p className="text-sm text-red-500"></p>
+                    </div>
+                    <label className="flex mt-2" htmlFor="password">Password:</label>
+                    <div className="relative">
+                        <input className="flex w-full p-2 my-1 rounded-lg shadow-xl ring ring-blue-500/50 "
+                               type="password" name="password" id="password" value=""/>
+                        <i className="absolute top-0 right-0 mt-3 mr-3 text-gray-500 far fa-eye hover:cursor-pointer"
+                           id="showPassword"></i>
+                        <p className="text-sm text-red-500"></p>
+                    </div>
+
+                    <div className="flex justify-between">
+                        <div>
+                            <input className="mr-1" type="checkbox" name="remember" id="remember" value="1"/>
+                            <label htmlFor="">Remember me</label>
                         </div>
-                        <div className="mb-5 relative">
-                            <label
-                                htmlFor="password"
-                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                            >
-                                Password
-                            </label>
-                            <i
-                                id="togglePassword"
-                                className="far fa-eye absolute right-[10px] top-[40px] text-gray-500 cursor-pointer"
-                            ></i>
-                            <input
-                                type="password"
-                                id="password"
-                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                                required
-                                placeholder="Password"
-                            />
-                        </div>
-                        <div className="flex items-start mb-5">
-                            <div className="flex items-center h-5">
-                                <input
-                                    id="remember"
-                                    type="checkbox"
-                                    value=""
-                                    className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 "
-                                    required
-                                />
-                            </div>
-                            <label
-                                htmlFor="remember"
-                                className="ms-2 text-sm font-medium text-gray-900 "
-                            >
-                                Remember me
-                            </label>
-                        </div>
-                        <button
-                            type="submit"
-                            className="mb-5 text-white bg-green-700 hover:bg-green-800 hover:cursor-pointer focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
-                        >
-                            Submit
-                        </button>
-                    </form>
-                    <div></div>
-                </div>
+                        <a href="">
+                            <p className="text-sm text-gray-500">Forgot your password?</p>
+                        </a>
+                    </div>
+                    <button type="submit" name="submit"
+                            className="bg-[#06859F] px-5 py-2 rounded-lg w-full font-semibold my-5 hover:cursor-pointer hover:bg-[#06AACC]">LogIn
+                    </button>
+                    <p className="text-sm text-center text-gray-500">Don't have an account! <a
+                        href=""><span className="text-blue-500 hover:text-blue-900">Register Here</span></a>
+                    </p>
+                </form>
+                <div></div>
             </div>
         </div>
+
     );
 };
 
